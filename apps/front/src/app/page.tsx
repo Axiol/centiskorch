31f72a46e1f287738e43f2a19732f04ec5a39fa4
Arnaud Delante <contact@arnaud.ninja>
2024-06-28 15:10:58 +0200

    feat: add empty days and checkboxes in the grid

--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -44,6 +44,22 @@ export default async function Home() {
           <div key={index}>
             {day.day}, {day.isWeekend ? "weekend" : "weekday"},{" "}
             {day.isDayOff ? "day off" : "work day"}
+            <div>
+              <div>
+                AM :{" "}
+                <input
+                  type={"checkbox"}
+                  defaultChecked={!(day.isDayOff || day.isWeekend)}
+                />
+              </div>
+              <div>
+                PM :{" "}
+                <input
+                  type={"checkbox"}
+                  defaultChecked={!(day.isDayOff || day.isWeekend)}
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
